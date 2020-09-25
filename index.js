388e085cfd76e8c79deaca8bcfb59556cb66bd30
Irina K <selezneva@xaker.ru>
2020-09-25 19:11:26 +0300

    ver 2.0

--- index.js
+++ index.js
@@ -1,53 +1,82 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiBaseUrl = 'http://api.tvmaze.com/';
-    const showId = '1363';
+    const searchInput = document.getElementById('search_input');
+    const searchShow = document.getElementById('search_show');
+    const container = document.getElementById('container');
+    const showPoster = document.getElementById('show_poster');
+    const showAboutContainer = document.querySelector('.show_about_container');
+    const seasonSelectorContainer = document.getElementById('season_selector_container');
+    const episodesContainer = document.getElementById('episodes_container');
+    const showNotExist = document.getElementById('show_not_exist');
+    let showEpisodes = [];
 
-    fetch(apiBaseUrl + 'shows/' + showId)
-        .then((resporse) => {
-            return resporse.json();
-        })
-        .then((show) => renderShow(show));
+    function onSubmit(event) {
+        if (searchInput.value !== '') {
+            fetch(apiBaseUrl + '/search/shows?q=' + searchInput.value)
+                .then((response) => response.json())
+                .then((showList) => getShow(showList[0]));
+            
+            searchInput.value = '';
+        }
+        event.preventDefault();
+    }
 
-    fetch(apiBaseUrl + 'shows/' + showId + '/episodes')
-        .then((resporse) => {
-            return resporse.json();
-        })
-        .then((episodes) => renderShowEpisodes(episodes));
+    searchShow.addEventListener('submit', onSubmit);
 
-    // let xhr = new XMLHttpRequest();
-    // let xhrEpisodes = new XMLHttpRequest();
-
-    // xhr.open('GET', apiBaseUrl + 'shows/' + showId);
-    // xhr.send();
-    // xhr.onload = function() {
-    //     let show = JSON.parse(xhr.response);
-    //     renderShow(show);
-    // };
-
-    // xhrEpisodes.open('GET', apiBaseUrl + 'shows/' + showId + '/episodes');
-    // xhrEpisodes.send();
-    // xhrEpisodes.onload = function() {
-    //     let showEpisodes = JSON.parse(xhrEpisodes.response);
-    //     renderShowEpisodes(showEpisodes);
-    // };
+    function getShow(show) {
+        if (show) {
+            fetch(apiBaseUrl + 'shows/' + show.show.id)
+                .then((response) => response.json())
+                .then((show) => renderShow(show));
+    
+            fetch(apiBaseUrl + 'shows/' + show.show.id + '/episodes')
+                .then((response) => response.json())
+                .then((episodes) => {
+                    showEpisodes = episodes;
+                    renderShowEpisodes();
+                });
+        } else {
+            showNotExist.style.display = 'block';
+            container.style.display = 'none';
+        };
+    }
 
     function renderShow(show) {
-        document.getElementById('showName').textContent = show.name;
-        document.getElementById('showPoster').setAttribute('src', show.image.original);
-        document.getElementById('showStatus').textContent = show.status;
-        document.getElementById('showGenres').textContent = show.genres.join(' | ');
-        document.getElementById('showRating').textContent = show.rating.average;
-        document.getElementById('showOfficialSite').setAttribute('href', show.officialSite);
-        document.getElementById('showOfficialSite').innerHTML = show.officialSite;
-        document.getElementById('showSummary').innerHTML = show.summary;
-    };
+        showNotExist.style.display = 'none';
+        container.style.display = 'block';
+        showAboutContainer.innerHTML = '';
+        showPoster.setAttribute('src', show.image.original);
 
-// ________________________________________________________________________________
-    function renderShowEpisodes(showEpisodes) {
-        let episodesContainer = document.getElementById('episodesContainer');
-        let seasonSelectorContainer = document.getElementById('seasonSelectorContainer');
+        document.getElementById('show_name').textContent = show.name;
+
+        let showStatus = document.createElement('div');
+        showStatus.textContent = 'status: ' + show.status;
+        showAboutContainer.append(showStatus);
+
+        let showGenres = document.createElement('div');
+        showGenres.textContent = 'genres: ' + show.genres.join(' | ');
+        showAboutContainer.append(showGenres);
+
+        let showRating = document.createElement('div');
+        showRating.innerHTML = 'rating:&nbsp;';
+        showAboutContainer.append(showRating);
+        let ratingValue = document.createElement('span');
+        ratingValue.textContent = show.rating.average || 'n/a';
+        showRating.append(ratingValue);
+
+        let showOfficialSite = document.createElement('a');
+        showOfficialSite.setAttribute('href', show.officialSite);
+        showOfficialSite.setAttribute('target', '_blank');
+        showOfficialSite.textContent = 'Official site';
+        showAboutContainer.append(showOfficialSite);
+
+        document.getElementById('show_summary').innerHTML = show.summary;
+    }
+
+    function renderShowEpisodes() {
         let lastSeason = 0;
-        // let episodesList = [];
+
+        seasonSelectorContainer.innerHTML = '';
 
         showEpisodes.forEach(function(episode) {
             if (episode.season > lastSeason) {
@@ -60,50 +89,50 @@ document.addEventListener('DOMContentLoaded', function() {
             season.setAttribute('data-season', i);
             season.textContent = i;
             seasonSelectorContainer.append(season);
-        };
+        }
 
-        seasonSelectorContainer.addEventListener('click', function(event) {
-            let activeSeason = event.target.dataset.season;
-            if (activeSeason !== null) {
-                renderSeason(activeSeason);
+        let defaultSeason = document.querySelector('.season_selector_container > button');
+        renderSeason(1, defaultSeason);
+    }
+
+    seasonSelectorContainer.addEventListener('click', function(event) {
+        let activeButton = event.target.closest('button');
+        if (activeButton) {
+            for (let i = 0; i < seasonSelectorContainer.children.length; i++) {
+                seasonSelectorContainer.children[i].classList.remove('active_season');
             }
-        });
+            activeButton.classList.add('active_season');
+            renderSeason(event.target.dataset.season);
+        }
+    });
 
-        function renderSeason(activeSeason) {
-            let episodesList = [];
-            episodesContainer.textContent = null;                      /* !!! */
-            showEpisodes.forEach(function(episodes) {
-                if (episodes.season == (activeSeason)) {
-                    // console.log(episodes);
-                    episodesList.push(episodes);
-                }
-            })
-            // console.log(episodesList);
-            showSeason(episodesList);
-        };
-
-        function showSeason(episodes) {
-            // console.log(episodes);
-            episodes.forEach(function(episode) {
+    function renderSeason(activeSeason) {
+        episodesContainer.innerHTML = '';
+        showEpisodes
+            .filter(episode => episode.season == activeSeason)
+            .forEach(function(episode) {
                 let showEpisode = document.createElement('div');
                 showEpisode.className = 'episode';
                 episodesContainer.append(showEpisode);
 
-                let showEpisodeNumber = document.createElement('p');
+                const showEpisodePoster = document.createElement('img');
+
+                (episode.image !== null) ?
+                showEpisodePoster.setAttribute('src', episode.image.medium) :
+                showEpisodePoster.setAttribute('src', 'assets/posternull.png');
+
+                showEpisodePoster.className = 'episode_poster';
+                showEpisode.append(showEpisodePoster);
+
+                const showEpisodeNumber = document.createElement('h6');
                 showEpisodeNumber.className = 'episode_number';
                 showEpisodeNumber.textContent = episode.number;
                 showEpisode.append(showEpisodeNumber);
 
-                let showEpisodePoster = document.createElement('img');
-                showEpisodePoster.setAttribute('src', episode.image.medium);
-                showEpisodePoster.className = 'episode_poster';
-                showEpisode.append(showEpisodePoster);
-
-                let showEpisodeSummary = document.createElement('p');
-                showEpisodeSummary.className = 'episode_summary';
-                showEpisodeSummary.innerHTML = episode.summary;
-                showEpisode.append(showEpisodeSummary);
+                const showEpisodeName = document.createElement('h6');
+                showEpisodeName.className = 'episode_name';
+                showEpisodeName.textContent = episode.name;
+                showEpisode.append(showEpisodeName);
             });
-        };
-    };
+    }
 });
